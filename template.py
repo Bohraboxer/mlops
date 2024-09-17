0936e7451b8230262e88e360963ada8a21fedf27
Bohraboxer <Rohitbohra23051994@gmail.com>
2024-09-17 22:56:19 +0530

    Updating folder/file structure

--- template.py
+++ template.py
@@ -13,12 +13,12 @@ list_of_files = [
     "src/pipeline/__init__.py",
     "src/pipeline/training_pipeline.py",
     "src/pipeline/prediction_pipeline.py",
-    "src/utils/__init__.py"
+    "src/utils/__init__.py",
     "src/utils/utils.py",
     "src/logger/logging.py",
-    "src/exception/exception"
+    "src/exception/exception",
     "tests/unit/__init__.py",
-    "tests/integration/__init__.py"
+    "tests/integration/__init__.py",
     "init_setup.sh",
     "requirements.txt",
     "requirements_dev.txt",
